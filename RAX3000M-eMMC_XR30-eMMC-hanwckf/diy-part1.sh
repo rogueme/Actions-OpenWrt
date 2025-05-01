eb65d26101d7e5277d877b054dc982f7f4f2187d
rogueme <37013377+rogueme@users.noreply.github.com>
2025-05-01 13:02:18 +0800

    Update diy-part1.sh

--- RAX3000M-eMMC_XR30-eMMC-hanwckf/diy-part1.sh
+++ RAX3000M-eMMC_XR30-eMMC-hanwckf/diy-part1.sh
@@ -17,7 +17,7 @@
 #添加easymesh
 #echo 'src-git luci-app-easymesh https://github.com/shuishihan/luci-app-easymesh.git' >> feeds.conf.default
 #添加luci-app-adguardhome
-echo 'src-git luci-app-adguardhome https://github.com/rufengsuixing/luci-app-adguardhome.git' >> feeds.conf.default
+#echo 'src-git luci-app-adguardhome https://github.com/rufengsuixing/luci-app-adguardhome.git' >> feeds.conf.default
 # istore
 echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
 echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
